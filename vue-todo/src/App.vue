bc9c386a6cea9aaa93e0e13b3421be1dcf7de89d
akira uemoto <empty.aauu0001@gmail.com>
2018-10-20 22:07:39 +0900

    mk todo

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,118 @@
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="todolist not-done">
+                    <h1>Todos</h1>
+                    <!-- <AddTodo @add-todo='addTodo'/> -->
+                    <AddTodo/>
+
+                    <hr>
+
+                    <!-- <TodoListView :todos='unFinishedTodos' @completed-todo='completedTodo'/> -->
+                    <TodoListView :todos='unFinishedTodos'/>
+                    <div class="todo-footer">
+                        <strong><span class="count-todos">{{unFinishedTodos.length}}</span></strong> Items Left
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <!-- <FinishedTodos :todos='finishedTodos' @delete-todo='deleteTodo'/> -->
+                <FinishedTodos :todos='finishedTodos'/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoListView from './components/TodoListView.vue';
+import AddTodo from './components/AddTodo.vue';
+import FinishedTodos from './components/FinishedTodos';
+import EventBus from './EventBus.js';
+
+import {todos} from './seed.js'
+export default {
+    name: 'app',
+    data(){
+      return {
+        todos:todos
+      }
+    },
+    components:{
+        TodoListView,
+        AddTodo,
+        FinishedTodos
+    },
+    created(){
+      EventBus.$on('add-todo', event => this.addTodo(event));
+      EventBus.$on('completed-todo', event => this.completedTodo(event));
+      EventBus.$on('delete-todo', event => this.deleteTodo(event));
+    },
+    methods:{
+      addTodo(event){
+        this.todos.push(event)
+      },
+      completedTodo(event){
+        const index = this.todos.findIndex(todo => todo.id === event.id);
+        this.todos[index].completed = true;
+      },
+      deleteTodo(event){
+        const index = this.todos.findIndex(todo => todo.id === event.id);
+        this.todos.splice(index, 1);
+      }
+    },
+    computed:{
+      finishedTodos(){
+        return this.todos.filter(todo => todo.completed === true)
+      },
+      unFinishedTodos(){
+        return this.todos.filter(todo => todo.completed === false)
+      }
+    }
+};
+</script>
+
+<style>
+body {
+	background-color: #eeeeee;
+}
+.todolist {
+	background-color: #fff;
+	padding: 20px 20px 10px 20px;
+	margin-top: 30px;
+}
+.todolist h1 {
+	margin: 0;
+	padding-bottom: 20px;
+	text-align: center;
+}
+.form-control {
+	border-radius: 0;
+}
+li.ui-state-default {
+	background: #fff;
+	border: none;
+	border-bottom: 1px solid #ddd;
+}
+li.ui-state-default:last-child {
+	border-bottom: none;
+}
+.todo-footer {
+	background-color: #f4fce8;
+	margin: 0 -20px -10px -20px;
+	padding: 10px 20px;
+}
+#done-items li {
+	padding: 10px 0;
+	border-bottom: 1px solid #ddd;
+	text-decoration: line-through;
+}
+#done-items li:last-child {
+	border-bottom: none;
+}
+#checkAll {
+	margin-top: 10px;
+}
+</style>
---
+++ vue-todo/src/components/AddTodo.vue
@@ -0,0 +1,20 @@
+<template>
+    <input type="text" class="form-control add-todo" placeholder="Add todo" v-model='title' @keyup.enter='saveTodo(title)'>
+</template>
+
+<script>
+import EventBus from '../EventBus.js'
+export default {
+  name: 'AddTodo',
+  data(){
+    return {
+      title:''
+    }
+  },
+  methods:{
+    saveTodo(title){
+      EventBus.$emit('add-todo',{title, id: Date.now().toString(), completed:false})
+    }
+  }
+}
+</script>
