aebb55fad2cf7c4fb8e64edd9320b569c3da1937
akira uemoto <empty.aauu0001@gmail.com>
2018-10-20 13:58:26 +0900

    mk kanban

---
+++ vue-kanban/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div id="app" class="container">
+        <Board />
+        <hr />
+        <AddTask />
+    </div>
+  </div>
+</template>
+
+<script>
+import Board from './components/Board.vue'
+import AddTask from './components/AddTask.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Board,
+    AddTask
+  }
+}
+</script>
+
+<style>
+.uk-card-body {
+	padding: 12px 30px;
+}
+.icons .fa {
+	padding: 0 2px;
+}
+.task-header {
+	text-align: center;
+	margin-top: 10px;
+}
+</style>
---
+++ vue-kanban/src/components/Board.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="container">
+      <h3 align="center" class="uk-heading-divider">Kanban Board</h3>
+      <div class="uk-grid-medium uk-child-width-expand@s uk-text-center" uk-grid>
+          <BoardColumn v-for='col in sharedState.Seed' :key='col.id' :col='col'/>
+      </div>
+  </div>
+</template>
+
+<script>
+import BoardColumn from './BoardColumn.vue';
+import {store} from '../store.js';
+
+export default {
+  name: 'Board',
+  data(){
+      return {
+        sharedState: store.state
+      }
+  },
+  components: {
+    BoardColumn
+  }
+}
+</script>
+
+<style>
+
+</style>
