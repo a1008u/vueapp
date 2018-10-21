abb09411d3a0bfa605741234d8f0924e3f670419
akira uemoto <empty.aauu0001@gmail.com>
2018-10-21 20:19:45 +0900

    mk vue-form

---
+++ vue-form/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <!-- <basic-form /> -->
+    <!-- <form-validations /> -->
+    <!-- <ValueBinding /> -->
+    <CustomValidations />
+  </div>
+</template>
+
+<script>
+import BasicForm from './components/BasicForm.vue'
+import FormValidations from './components/FormValidations.vue'
+import ValueBinding from './components/ValueBinding.vue'
+import CustomValidations from './components/CustomValidations.vue'
+
+export default {
+  name: 'app',
+  components: {
+    BasicForm,
+    FormValidations,
+    ValueBinding,
+    CustomValidations
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
