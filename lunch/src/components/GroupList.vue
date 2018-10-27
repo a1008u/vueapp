bf4928003e6b6fad59e096601f4131d78058dfa2
akira uemoto <empty.aauu0001@gmail.com>
2018-10-28 01:26:30 +0900

    mk ck2

--- lunch/src/components/GroupList.vue
+++ lunch/src/components/GroupList.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="groupList">
-    <ul v-for="(members, index) in mkGroup" :key='index'>
+    <ul v-for="(members, index) in members" :key='index'>
       <div v-for="(member, index) in members" :key='index'>
         {{member.name}}
       </div>
@@ -11,7 +11,7 @@
 <script>
 import {members} from '../group/member.js'
 import {mapGetters} from 'vuex';
-
+import axios from 'axios';
 
 export default {
   name: 'grouplist',
@@ -22,77 +22,79 @@ export default {
   },
   computed:{
     ...mapGetters(['members']),
-    mkGroup(){
+    // mkGroup(){
+    //   let newMembers=[];
+    //   let copyMs = []
+    //   members.forEach(m => {
+    //     copyMs.push(m);
+    //   })
+    //   console.table(copyMs)
 
-      let newMembers=[];
-      let copyMs = []
-      members.forEach(m => {
-        copyMs.push(m);
-      })
+    //   // 過去分取得
+    //   let pastGroup = this.$store.getters.members;
+    //   let group = [];
+    //   let mem = [];
+    //   let tempG = []
+    //   while(newMembers.length !== 10){
 
-      // 過去分取得
-      let pastGroup = this.$store.getters.members;
-      let group = [];
-      let mem = [];
-      let tempG = []
-      while(newMembers.length !== 10){
+    //     if(group.length !== 4) {
+    //       let random = Math.floor(Math.random() * copyMs.length);
+    //       let index = -1;
 
-        if(group.length !== 4) {
-          let random = Math.floor(Math.random() * copyMs.length);
-          let index = -1;
+    //       if (pastGroup.length !== 0) {
+    //         let v = pastGroup.length
+    //         for (let i = 0; i<v; i++) {
+    //           console.log(' ===== ')
+    //           pastGroup[i].find(m => {
+    //             if(m.name === copyMs[random].name) {
+    //               index = i;
+    //             }
+    //           })
 
-          if (pastGroup.length !== 0) {
-            let v = pastGroup.length
-            for (let i = 0; i<v; i++) {
-              console.log(' ===== ')
-              pastGroup[i].find(m => {
-                if(m.name = copyMs[random].name) {
-                  index = i;
-                }
-              })
+    //           if (index !== -1) {
+    //             break;
+    //           }
+    //         }
 
-              if (index !== -1) {
-                break;
-              }
-            }
+    //         if(index !== -1) {
+    //           let pG = pastGroup[index];
+    //           for(let g of pG) {
+    //             tempG.push(g.name);
+    //           }
+    //         }
+    //       }
 
-            if(index !== -1) {
-              let pG = pastGroup[index];
-              for(let g of pG) {
-                tempG.push(g.name);
-              }
-            }
-          }
+    //       if(tempG.length === 0) {
+    //           mem = copyMs.splice(random, 1);
+    //           if(mem[0]) {
+    //             group.push(mem[0].name)
+    //           }
+    //       } else {
+    //         group.concat(tempG).forEach(i =>{
+    //           if(group.includes(i) && tempG.includes(i)) {
+    //             console.log(group.includes(i) + " " + tempG.includes(i))
+    //           } else {
+    //             mem = copyMs.splice(random, 1);
+    //             if(mem[0]) {
+    //               group.push(mem[0].name)
+    //             }
+    //           }
+    //         })
+    //       }
+    //     } else {
+    //       let gs = [];
+    //       group.forEach(g => {
+    //         gs.push({name: g})
+    //       })
+    //       newMembers.push(gs);
+    //       mem = [];
+    //       group = [];
+    //     }
+    //   }
 
-          if(tempG.length === 0) {
-              mem = copyMs.splice(random, 1);
-              console.log(mem[0].name)
-              group.push(mem[0].name)
-          } else {
-            group.concat(tempG).forEach(i =>{
-              if(group.includes(i) && tempG.includes(i)) {
-                console.log(group.includes(i) + " " + tempG.includes(i))
-              } else {
-                // mem = copyMs.splice(random, 1);
-                // console.log(copyMs)
-                //group.push(mem[0].name)
-              }
-            })
-          }
-        } else {
-          let gs = [];
-          group.forEach(g => {
-            gs.push({name: g})
-          })
-          newMembers.push(gs);
-          mem = [];
-          group = [];
-        }
-      }
-
-      this.tempMembers = newMembers;
-      return newMembers;
-    }
+    //   this.tempMembers = newMembers;
+    //   return newMembers;
+    // }
   },
   created(){
     this.$store.dispatch('fetchMembers');
--- lunch/src/store/GroupList.js
+++ lunch/src/store/GroupList.js
@@ -1,19 +1,91 @@
 import axios from 'axios';
 import { API_URL } from '../constants';
+import { importMembers } from '../group/member.js'
 
 const state = {
 	members: []
 };
 
-const getters = {
-	members(state){
-		return state.members;
-	}
-};
+
 
 const mutations = {
 	UPDATE_MEMBERS(state, payload){
 		state.members = payload;
+	}
+};
+
+const getters = {
+	members(state){
+
+		let newMembers = [];
+		let newGroup = [];
+		let dabuleCkGroup = [];
+		while(newMembers.length !== 2){
+
+			// 新規グループ作成
+			let random = Math.floor(Math.random() * importMembers.length);
+			let temp = importMembers[random].name
+			if(newGroup.length === 0) {
+				newGroup.push(temp)
+				importMembers.splice(random, 1);
+			} else {
+
+				// 重複確認ロジック
+				// 発見
+				if(newGroup.length > 1) {
+					console.log('ddddddd')
+					
+					state.members.forEach(group => {
+						if(dabuleCkGroup.length === 0) {
+							group.some((member, index) => {
+								if (newGroup.includes(member.name)){
+									state.members[index].forEach(tempMember =>{
+										if(tempMember.name !== member.name) {
+											dabuleCkGroup.push(tempMember.name);
+										}
+									});
+									return true;
+								}
+							})
+						}
+					});
+				}
+
+				// 重複チェック
+				let ck = false;
+				if(dabuleCkGroup.length !== 0) {
+					console.log(dabuleCkGroup)
+					newGroup.concat(dabuleCkGroup).forEach(i =>{
+						if(newGroup.includes(i) && dabuleCkGroup.includes(i)) {
+							ck = true;
+						} else {
+							newGroup.push(temp)
+							importMembers.splice(random, 1);
+						}
+					})
+				} else {
+					newGroup.push(temp)
+					importMembers.splice(random, 1);
+				}
+
+				if(newGroup.length === 4) {
+					if(ck) {
+						dabuleCkGroup = [];
+						newGroup = [];
+					} else {
+						let tmp = [];
+						newGroup.forEach(newMember => {
+							tmp.push({name: newMember})
+						})
+						newMembers.push(tmp);
+						dabuleCkGroup = [];
+						newGroup = [];
+					}
+				}
+			}
+		}
+		console.table(newMembers)
+		return newMembers;
 	}
 };
 
